b367aac6e47441fe44c24f7dd391d96959032424
HuataoWen421 <60019549+HuataoWen421@users.noreply.github.com>
2020-03-15 21:34:44 -0700

    nva bar

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello Joseph smileccc');
+   res.send('Hello Joseph dddd');
 })
 
 var server = app.listen(4000, function () {
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,40 +1,58 @@
-import React, { useState } from 'react';
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
+import React from 'react'; 
+import { Navbar, Nav, NavItem, NavLink, Form, FormControl, Button } from 'react-bootstrap';
+import Select from 'react-select';
+import Switch from "react-switch";
 import './App.css';
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
 
+const App = (props) => {
+  handleChange(checked) {
+    this.setState({ checked });
+  }
   return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
-};
+    
+    <div>
+      <Navbar bg="dark" variant="dark" expand="md">
+        <div style={{width: '300px'}}>
+          <Select
+        name="colors"
+        className="basic-multi-select"
+        classNamePrefix="select"
+        options={options}
+        placeholder="Enter keyword .."
+      />
+      </div>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      
+    
+    
+      
+      <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto" navbar>
+            <NavItem><NavLink href="/components/">Home</NavLink></NavItem>
+            <NavItem><NavLink href="/components/">World</NavLink></NavItem>
+            <NavItem><NavLink href="/components/">Politics</NavLink></NavItem>
+            <NavItem><NavLink href="/components/">Business</NavLink></NavItem>
+            <NavItem><NavLink href="/components/">Technology</NavLink></NavItem>
+            <NavItem><NavLink href="/components/">Sports</NavLink></NavItem>
 
-const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Welcodddme To ddd-ddddBootstrap</h1>
-      <ExampleToast className="toast">
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
-);
+            <label>
+        <span>Switch with default style</span>
+        <Switch onChange={this.handleChange} checked={this.state.checked} />
+      </label>
+          </Nav>
+          
+        </Navbar.Collapse>
+
+        
+      </Navbar>
+    </div>
+  );
+}
 
 export default App;
